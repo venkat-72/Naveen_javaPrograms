e3f294463d2bcdaa0be068a6ec07be0607ec0236
venkat-72 <pasumarthivenkatanaveen72@gmail.com>
2023-06-14 11:26:00 -0500

    threads

---
+++ src/com/Week3/Day02/ThreadClass.java
@@ -0,0 +1,15 @@
+package com.Week3.Day02;
+
+public class ThreadClass extends Thread{
+    public void run(){
+        for(int i=1;i<=10;i++){
+            System.out.println(i);
+        }
+    }
+
+    public static void main(String[] args){
+        ThreadClass t = new ThreadClass();
+        t.start();
+    }
+
+}
