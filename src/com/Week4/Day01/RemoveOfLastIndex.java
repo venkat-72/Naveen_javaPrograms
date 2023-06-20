7289df93137e085f210678911dab9f10484bfe21
venkat-72 <pasumarthivenkatanaveen72@gmail.com>
2023-06-20 13:32:30 -0500

    collections

---
+++ src/com/Week4/Day01/RemoveOfLastIndex.java
@@ -0,0 +1,22 @@
+package com.Week4.Day01;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RemoveOfLastIndex {
+    public static void main(String[] args) {
+        List<String> stringList = new ArrayList<>();
+        stringList.add("Cricket");
+        stringList.add("football");
+        stringList.add("basketball");
+        stringList.add("hockey");
+
+
+        System.out.println("before removing the last element" + stringList );
+
+        int lastIndex = stringList.size() - 1;
+        stringList.remove(lastIndex);
+        System.out.println("After removing the last element: " + stringList);
+
+    }
+}
---
+++ src/com/Week4/Day01/Students.java
@@ -0,0 +1,26 @@
+package com.Week4.Day01;
+
+public class Students {
+    private String FN;
+    private String LN;
+    private double GPA;
+
+    public Students(String FN, String LN, double GPA) {
+        this.FN = FN;
+        this.LN = LN;
+        this.GPA = GPA;
+    }
+
+    public String getFN() {
+        return FN;
+    }
+
+    public String getLN() {
+        return LN;
+    }
+
+    public double getGPA() {
+        return GPA;
+        }
+    }
+
